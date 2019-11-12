0e28e5333faa647bfce197f0a0936cd2603b73ca
Ilham Rahman <ilhaamrahman@gmail.com>
2019-11-12 13:45:25 +0900

    create user login signup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show] # we add them later :edit, :update
-  
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @events = Event.all
   end
