2f2e014ac4e9018e0c86680a56c49f4eee52cb25
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-11-13 11:55:49 +0900

    add more seeds to movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,18 @@
 require "json"
 require "open-uri"
 
-url = 'http://www.omdbapi.com/?i=tt3896198&apikey=c799fbe5&s=star+wars'
-data = open(url).read
-movies = JSON.parse(data)
+keywords = ['star+wars', 'harry+potter', 'fast+and+furious', 'pokemon']
+info = []
 
-movies["Search"].each do |movie|
-    Movie.create!(name: movie["Title"], photo: movie["Poster"], genre: "Science Fiction")
+keywords.each do |key|
+  url = "http://www.omdbapi.com/?i=tt3896198&apikey=c799fbe5&s=#{key}"
+  data = open(url).read
+  movies = JSON.parse(data)
+  info << movies["Search"]
 end
 
-
-
+info.each do |array|
+  array.each do |movie|
+    Movie.create!(name: movie["Title"], photo: movie["Poster"], genre: "Science Fiction")
+  end
+end
