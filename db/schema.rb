3414212253940692eef3b4c607e330232240a943
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2019-11-14 21:43:47 +0900

    compelted user profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_025338) do
+ActiveRecord::Schema.define(version: 2019_11_14_105148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,17 @@ ActiveRecord::Schema.define(version: 2019_11_14_025338) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
+    t.bigint "event_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_reviews_on_event_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_025338) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "photo"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -68,4 +80,6 @@ ActiveRecord::Schema.define(version: 2019_11_14_025338) do
   add_foreign_key "bookings", "users"
   add_foreign_key "events", "movies"
   add_foreign_key "events", "users"
+  add_foreign_key "reviews", "events"
+  add_foreign_key "reviews", "users"
 end
